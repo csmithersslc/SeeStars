9785d59558952307e66607534a8e53cb3b11fd12
Christina Smithers <christina.smithers@gmail.com>
2016-09-08 21:07:43 -0600

    More functional refactoring
    Explored the ideas of functional programming and refactoring more.  Also added new capabilities for rocketries from Mars and the moon.

---
+++ lib/calcs.ex
@@ -0,0 +1,7 @@
+#to double-check the math stuff
+
+defmodule Calcs do
+	def square_root(val), do: :math.sqrt(_)
+	def squared(val), do: val * val
+	def cubed(val), do: val * val * val
+end
--- lib/rocketry.ex
+++ lib/rocketry.ex
@@ -1,40 +1,28 @@
-defmodule Converter do
-	def to_nearest tenth(val) do
-		Float.ceil(val, 1)
-	end
-
-	def to_km(velocity) do
-		velocity / 1000
-	end
-
-	def to_light_seconds({:miles, miles}) do
-		(miles * 5.36819e-6) |> round_down
-	end
-
-	def to_light_seconds({:meters, meters}) do
-		(meters * 3.3356.8620368e-9) |> round_down
-	end
-
-	def round_down(val) when is_float(val), do: trunc(val)
-end
-
 defmodule Physics.Rocketry do
 
-	def escape_velocity(:earth) do
-		%{mass: 5.972e24, radius: 6.371e6}
-			|> escape_velocity
-	end
+	import Converter
+	import Calcs
+	import Physics.physics_laws
+	import Bodies
+
+	#moved data to other files to understand module attributes
+	#module attributes - NO reassigning and NOT properties, only values in compilation time
+	#pattern-matching for Xtina being a simpleton - testing to see how things change 
+	#with each planet's values
+
+	def escape_velocity(:earth) do: @earth |> escape_velocity
+	def escape_velocity(:moon) do: @moon |> escape_velocity
+	def escape_velocity(:mars) do: @mars|> escape_velocity
 	
 	def escape_velocity(planet) when is_map(planet) do
 		planet
 			|> calculate_escape
-			|> Converter.to_km
-			|> Converter.to_nearest_tenth
+			|> to_km
+			|> to_nearest_tenth
 	end
 
 	def calculate_escape(%{mass: mass, radius: radius}) do
 		newtons_constant = 6.67e-11
-		2 * newtons_constant * mass / radius
-			|> :math.sqrt(_)
-	end
+		2 * newtons_gravity_consta * mass / radius
+			|> square_root
 end
