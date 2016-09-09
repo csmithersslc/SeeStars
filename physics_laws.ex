968c8174886bd74dd9f166d8581a3b0f34545442
Christina Smithers <christina.smithers@gmail.com>
2016-09-08 21:06:18 -0600

    More FP explorations and refactoring
    In exploring the new ideas of functional programming and what Elixir can do, there was some refactoring done to make this more functional by breaking things down.

---
+++ physics_laws.ex
@@ -0,0 +1,5 @@
+# we have to make sure we are not inventing new physics and obeying all laws since we might crash or get high-jacked or something, so this is good to know
+
+defmodule Physics.Laws do
+	def newtons_gravity_constrant, do: 6.67e-11	
+end
---
+++ rocketry.ex
@@ -0,0 +1,28 @@
+defmodule Physics.Rocketry do
+
+	import Converter
+	import Calcs
+	import Physics.physics_laws
+	import Bodies
+
+	#moved data to other files to understand module attributes
+	#module attributes - NO reassigning and NOT properties, only values in compilation time
+	#pattern-matching for Xtina being a simpleton - testing to see how things change 
+	#with each planet's values
+
+	def escape_velocity(:earth) do: @earth |> escape_velocity
+	def escape_velocity(:moon) do: @moon |> escape_velocity
+	def escape_velocity(:mars) do: @mars|> escape_velocity
+	
+	def escape_velocity(planet) when is_map(planet) do
+		planet
+			|> calculate_escape
+			|> to_km
+			|> to_nearest_tenth
+	end
+
+	def calculate_escape(%{mass: mass, radius: radius}) do
+		newtons_constant = 6.67e-11
+		2 * newtons_gravity_consta * mass / radius
+			|> square_root
+end
